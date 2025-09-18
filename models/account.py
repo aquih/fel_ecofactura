3e4f6f2dd34d24529537fe3876dc96b7a04f7996
rocketgithub <rodrigo@aquih.com>
2025-09-18 11:45:42 -0600

    Usar https

--- models/account.py
+++ models/account.py
@@ -234,7 +234,7 @@ class AccountMove(models.Model):
 
                 wsdl = "https://www.facturaenlineagt.com/adocumento?wsdl"
                 if factura.company_id.pruebas_fel:
-                    wsdl = "http://pruebasecofactura.com:8443/fel/adocumento?wsdl"
+                    wsdl = "https://pruebasecofactura.com:8443/fel/adocumento?wsdl"
                 client = zeep.Client(wsdl=wsdl)
 
                 resultado = client.service.Execute(factura.company_id.vat, factura.company_id.usuario_fel, factura.company_id.clave_fel, factura.company_id.vat, xmls)
@@ -263,7 +263,7 @@ class AccountMove(models.Model):
                 
                 wsdl = "https://www.facturaenlineagt.com/aanulacion?wsdl"
                 if factura.company_id.pruebas_fel:
-                    wsdl = "http://pruebasecofactura.com:8443/fel/aanulacion?wsdl"
+                    wsdl = "https://pruebasecofactura.com:8443/fel/aanulacion?wsdl"
                 client = zeep.Client(wsdl=wsdl)
                 
                 resultado = client.service.Execute(factura.company_id.vat, factura.company_id.usuario_fel, factura.company_id.clave_fel, factura.company_id.vat, factura.firma_fel, factura.motivo_fel)
